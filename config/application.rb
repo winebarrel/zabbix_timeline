175e1f07fa02b0980a7c7e25638b15bd8b5f73e6
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-12-05 20:36:18 +0900

    Add timeline.js/css

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ZabbixTimeline
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.precompile << 'timeline.js'
+    config.assets.precompile << 'timeline.css'
   end
 end
