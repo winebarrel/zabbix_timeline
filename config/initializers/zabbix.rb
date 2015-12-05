22593f6b358bffefcb671812d38a52ee11d3dfa0
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-12-05 18:25:36 +0900

    Fix zabbix initializing

--- config/initializers/zabbix.rb
+++ config/initializers/zabbix.rb
@@ -1,5 +1,13 @@
-require 'zabbix/client'
-
-Rails.application.config.zabbix = ActiveSupport::HashWithIndifferentAccess.new(
+ActiveSupport::HashWithIndifferentAccess.new(
   YAML.load_file(Rails.root.join('config/zabbix.yml'))
-)
+).tap do |config|
+  zabbix_options = ActiveSupport::OrderedOptions.new
+  Rails.application.config.zabbix = zabbix_options
+
+  zabbix_options.config = config
+
+  client = Zabbix::Client.new(config[:endpoint], config)
+  zabbix_options.client = client
+
+  client.user.login(user: config[:user], password: config[:password])
+end
